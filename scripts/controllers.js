2d79582129fed42ef19a6f6885414add9bacea81
softdevelop <softdevelop.inc@gmail.com>
2019-10-11 17:38:58 +0700

    create photo grid show 30 photos ordered by name, responsive design

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,6 +1,8 @@
 // Define controller and the objects need to reference.
-myAngularApp.controller('appController', function ($scope) {
+myAngularApp.controller('appController', ['$scope', 'orderByFilter', function ($scope, orderBy) {
 	$scope.tab = 1;
+
+    $scope.photos = orderBy(photos, 'name');
 
     $scope.setTab = function(newTab){
       $scope.tab = newTab;
@@ -9,4 +11,4 @@ myAngularApp.controller('appController', function ($scope) {
     $scope.isSet = function(tabNum){
       return $scope.tab === tabNum;
     };
-});
+}]);
