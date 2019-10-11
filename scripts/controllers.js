b54da955145e301cb5cf1e0f88abd878c34f2d30
softdevelop <softdevelop.inc@gmail.com>
2019-10-11 15:42:47 +0700

    update style, nav, create tabs

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,4 +1,12 @@
 // Define controller and the objects need to reference.
 myAngularApp.controller('appController', function ($scope) {
+	$scope.tab = 1;
 
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
 });
